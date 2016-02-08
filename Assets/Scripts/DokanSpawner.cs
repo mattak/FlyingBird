9ae7deb2181967c509d3185dd2d669c973c17e18
mattak <mattak.me@gmail.com>
2016-02-08 23:37:58 +0900

    Create dokan spawner

---
+++ Assets/Scripts/DokanSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DokanSpawner : MonoBehaviour {
+	public GameObject prefab;
+	public float intervalSecond = 1.0f;
+	public float randomOffsetY = 5f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Spawn");
+	}
+
+	IEnumerator Spawn() {
+		while(true) {
+			float differenceY = (Random.value - 0.5f) * randomOffsetY;
+			Vector3 difference = new Vector3(0, differenceY, 0);
+			Instantiate(prefab, this.transform.position + difference, prefab.transform.rotation);
+			yield return new WaitForSeconds(intervalSecond);
+		}
+	}
+}
---
+++ Assets/Scripts/IntervalSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntervalSpawner : MonoBehaviour {
+	public GameObject prefab;
+	public float intervalSecond = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Spawn");
+	}
+
+	IEnumerator Spawn() {
+		while(true) {
+			Instantiate(prefab, this.transform.position, prefab.transform.rotation);
+			yield return new WaitForSeconds(intervalSecond);
+		}
+	}
+}
