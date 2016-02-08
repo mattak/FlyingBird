49687a72b1b7fe55fc2814612161b2d84f302558
mattak <mattak.me@gmail.com>
2016-02-08 23:49:58 +0900

    Reload when detect enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	private Collider2D collider;
+
+	void Start () {
+		this.collider = this.GetComponent<Collider2D>();
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
