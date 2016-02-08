a5ecca924673818296ca881877327b412c365792
mattak <mattak.me@gmail.com>
2016-02-08 23:07:26 +0900

    Place dokan

---
+++ Assets/Scripts/Dokan.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dokan : MonoBehaviour {
+	public float speed;
+
+	void Update () {
+		this.transform.Translate(new Vector3(-Time.deltaTime * speed, 0, 0));
+	}
+}
