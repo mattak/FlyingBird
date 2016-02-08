62b2a8d4039045bda88bbcad28a387db5501cb69
mattak <mattak.me@gmail.com>
2016-02-08 22:44:46 +0900

    Add bird velocity

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+	public float forceY;
+	public float clampVelocity;
+	private Rigidbody2D rigidbody;
+
+	void Start () {
+		this.rigidbody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		// 速度制限を設ける
+		this.rigidbody.velocity = Vector3.ClampMagnitude(this.rigidbody.velocity, clampVelocity);
+
+		if (Input.GetKeyDown("space")) {
+			this.MoveUp();
+		}
+	}
+
+	void MoveUp() {
+		// 速度をゼロにする
+		this.rigidbody.velocity = Vector3.zero;
+
+		// 上方向(Y)に力を加える
+		this.rigidbody.AddForce(new Vector2(0, forceY));
+	}
+}
